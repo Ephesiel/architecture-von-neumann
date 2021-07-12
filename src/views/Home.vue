ac3f4d78f6c8db60b08a168f6756d3e9b953dc33
Huftier Benoît <ephesiel@gmail.com>
2021-07-12 11:34:36 +0200

    Modification espace travail
    - Avant de travailler, ajout de quelques modification
    - ESLint ons save avec le fichier requis. Tous les fichiers ont été update avec a nouvelle config
    - Suppression de la page HelloWorld basique de vuejs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <Debug msg="Welcome to Your Vue.js App" />
+    </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import Debug from '@/components/Debug.vue'
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
+    name: 'Home',
+    components: {
+        Debug,
+    },
+}
 </script>
