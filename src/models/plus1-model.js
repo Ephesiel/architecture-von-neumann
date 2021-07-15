729179ac4be390ee6bebad8dd8ef2fdfb9389870
Johann Rosain <johann.rosain@etu.umontpellier.fr>
2021-07-15 12:39:17 +0200

    Ajout du composant Plus1
    * Implémentation du composant Plus1 du séquenceur.

---
+++ src/models/plus1-model.js
@@ -0,0 +1,32 @@
+import Clock from '@/models/clock.js'
+
+/**
+ * Implémentation du composant du séquenceur Plus1.
+ *
+ * Ce composant prend la sortie du registre d'accès à la mémoire de
+ * microprogrammation et lui ajoute 1, pour ensuite réinjecter cette valeur
+ * dans le multiplexeur dédié.
+ */
+export default class Plus1 {
+    // ------------------------------------------------------------------------
+    // Attributs.
+    input //: Bus
+    output //: Bus
+
+    // ------------------------------------------------------------------------
+    // Constructeur.
+
+    constructor(input, output) {
+        Clock.register(this.update.bind(this))
+
+        this.input = input
+        this.output = output
+    }
+
+    // ------------------------------------------------------------------------
+    // Méthodes publiques.
+
+    update() {
+        this.output.setValue(this.input.getValue() + 1n)
+    }
+}
