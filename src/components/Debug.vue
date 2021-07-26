93ce5cdf636b6088903061a5b1d16a4a7bd3a869
Johann Rosain <johann.rosain@etu.umontpellier.fr>
2021-07-26 14:47:42 +0200

    Placement des différents registres dynamiquement sur l'archi

--- src/components/Debug.vue
+++ src/components/Debug.vue
@@ -11,19 +11,91 @@
         :height="height"
         xmlns="http://www.w3.org/2000/svg"
     >
-        <Sequencer :sequencer-model="arch.sequencer"></Sequencer>
+        <!-- L1 RA, RB, RC -->
+        <Register
+            :register-model="arch.RA"
+            :width="componentWidth"
+            :height="componentHeight"
+            :x="x(0.5)"
+            :y="y(0)"
+        />
+        <Register
+            :register-model="arch.RB"
+            :width="componentWidth"
+            :height="componentHeight"
+            :x="x(1.5)"
+            :y="y(0)"
+        />
+        <Register
+            :register-model="arch.RC"
+            :width="componentWidth"
+            :height="componentHeight"
+            :x="x(2.5)"
+            :y="y(0)"
+        />
+
+        <!-- L2 CO RX SP RI -->
+        <Register
+            :register-model="arch.CO"
+            :width="componentWidth"
+            :height="componentHeight"
+            :x="x(1)"
+            :y="y(1)"
+        />
+        <Register
+            :register-model="arch.RX"
+            :width="componentWidth"
+            :height="componentHeight"
+            :x="x(2)"
+            :y="y(1)"
+        />
+        <Register
+            :register-model="arch.SP"
+            :width="componentWidth"
+            :height="componentHeight"
+            :x="x(3)"
+            :y="y(1)"
+        />
+
+        <!--
+        <Register
+            :register-model="arch.RI"
+            :width="componentWidth"
+            :height="componentHeight"
+            :x="x(3)"
+            :y="50"
+        />
+        <Sequencer :sequencer-model="arch.sequencer"></Sequencer>-->
+
+        <!-- L3 RAM Mémoire RE -->
+        <Register
+            :register-model="arch.RAM"
+            :width="componentWidth"
+            :height="componentHeight"
+            :x="x(0.5)"
+            :y="y(2)"
+        />
+        <Register
+            :register-model="arch.RE"
+            :width="componentWidth"
+            :height="componentHeight"
+            :x="x(2.5)"
+            :y="y(2)"
+        />
         Désolé, votre navigateur ne supporte pas le SVG.
     </svg>
 </template>
 
 <script>
 import Architecture from '@/models/von-neumann-architecture-model'
-import Sequencer from '@/components/Sequencer.vue'
+//import Sequencer from '@/components/Sequencer.vue'
+import Register from '@/components/Register.vue'
 
 export default {
     name: 'Debug',
     components: {
-        Sequencer,
+        //Sequencer,
+        Register,
     },
     data() {
         return {
@@ -36,6 +108,34 @@ export default {
         },
         height() {
             return this.$store.state.svgHeight
+        },
+        componentWidth() {
+            // 5 composants sur une ligne + marge d'un composant à gauche et à droite
+            const tot = this.width / 6
+            return tot - 0.1 * tot
+        },
+        componentHeight() {
+            // Au maximum, l'architecture doit faire 2/3 de la page ?, et 4 composants par colonne
+            const tot = this.height / 8
+            return tot - 0.1 * tot
+        },
+    },
+    methods: {
+        x(n) {
+            // Marges horizontales
+            return (
+                this.componentWidth / 2 +
+                n * this.componentWidth +
+                n * 0.1 * (this.width / 6)
+            )
+        },
+        y(n) {
+            // Marges verticales
+            return (
+                this.componentHeight / 2 +
+                n * this.componentHeight +
+                n * 0.1 * (this.height / 2)
+            )
         },
     },
 }
