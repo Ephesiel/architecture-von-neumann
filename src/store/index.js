a1bf1e8415ef14fce2d8388d11177284d401f03a
Huftier Benoît <ephesiel@gmail.com>
2021-07-29 16:00:01 +0200

    Le svg est resize lorsque la fenêtre l'est
    - Correction d'un bug sur les registres dont le texte ne changeait pas
    de taille

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import { createStore } from 'vuex'
 import SignalManager from '@/models/signal-manager'
 
-export default createStore({
+const page = document.getElementsByTagName('html')[0]
+
+const store = createStore({
     state: {
-        svgWidth: window.screen.availWidth,
-        svgHeight: window.screen.availHeight,
+        svgWidth: page.getBoundingClientRect().width,
+        svgHeight: page.getBoundingClientRect().height,
         signals: Object.fromEntries(
             Object.entries(SignalManager.getSignals()).map((o) => {
                 return [o[0], o[1] !== 0]
@@ -20,7 +22,19 @@ export default createStore({
         addSignal: (state, signal) => {
             state.signals[signal] = true
         },
+        changeSize: (state, page) => {
+            state.svgWidth = page.getBoundingClientRect().width
+            state.svgHeight = page.getBoundingClientRect().height
+        },
     },
     actions: {},
     modules: {},
 })
+
+const resizeObserver = new ResizeObserver(function () {
+    store.commit('changeSize', page)
+})
+
+resizeObserver.observe(page)
+
+export default store
