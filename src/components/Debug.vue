3d2ad09839fcec002c36b4213bbccc4cbf844326
Huftier Benoît <ephesiel@gmail.com>
2021-08-17 08:31:21 +0200

    Correction de quelques erreurs de merge
    - Les json devraient être formattés sans prettier pour plus de lisibilité
    - Le Debug marche désormais avec la clock pour plus de possibilité de
    test de composants
    - Ajout d'une méthode permettant de retrouver le nom d'un signal avec sa
    clé

--- src/components/Debug.vue
+++ src/components/Debug.vue
@@ -1,75 +1,48 @@
 <template>
-    <!--
-    <button @click="arch.stepByStep()">Pas à pas</button><br />
-    <button @click="arch.phaseByPhase()">Phase par phase</button>-->
     <svg
         version="1.1"
         baseProfile="full"
-        :width="realWidth"
-        :height="realHeight"
+        width="300"
+        height="300"
         xmlns="http://www.w3.org/2000/svg"
     >
-        <InstructionRegister
-            :register-model="arch.RI"
-            :width="componentWidth"
-            :height="componentHeight"
-            :sequencer-model="arch.sequencer"
-            :x="750"
-            :y="50"
-            :font-size="14"
-        />
         Désolé, votre navigateur ne supporte pas le SVG.
     </svg>
-    <button @click="arch.stepByStep()">Pas à pas</button><br />
-    <button @click="arch.phaseByPhase()">Phase par phase</button>
+    <div>
+        <button @click="stepByStep()">Pas à pas</button><br />
+        <button @click="phaseByPhase()">Phase par phase</button>
+    </div>
 </template>
 
 <script>
 import Architecture from '@/models/von-neumann-architecture-model'
-import InstructionRegister from '@/components/InstructionRegister.vue'
+import Clock from '@/models/clock'
 
 export default {
     name: 'Architecture',
-    components: {
-        InstructionRegister,
-    },
+    components: {},
     data() {
         return {
             arch: new Architecture(),
-            width: 200,
-            height: 150,
         }
     },
-    computed: {
-        componentWidth() {
-            return 400
-        },
-        componentHeight() {
-            return 50
-        },
-        realWidth() {
-            return this.$store.state.pageWidth
-        },
-        realHeight() {
-            return this.realWidth * (this.height / this.width)
-        },
+    created() {
+        Clock.register((UTA, signals) => {
+            for (const signal of Object.keys(signals)) {
+                if (signals[signal] > 0) {
+                    this.$store.commit('addSignal', signal)
+                }
+            }
+        })
     },
     methods: {
-        x(n) {
-            // Marges horizontales
-            return (
-                this.componentWidth / 2 +
-                n * this.componentWidth +
-                n * 0.1 * (this.width / 6)
-            )
+        stepByStep() {
+            this.$store.commit('resetSignals')
+            this.arch.stepByStep()
         },
-        y(n) {
-            // Marges verticales
-            return (
-                this.componentHeight / 2 +
-                n * this.componentHeight +
-                n * 0.1 * (this.height / 2)
-            )
+        phaseByPhase() {
+            this.$store.commit('resetSignals')
+            this.arch.phaseByPhase()
         },
     },
 }
