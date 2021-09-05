4e5c550b81cc96bd0e0b68435083102e1c88392d
Johann Rosain <johann.rosain@gmail.com>
2021-09-05 17:15:32 +0200

    Factorisation des textes flottants
    * Nouvelle vue : FloatingText, pour les textes flottants dans un svg.
    Conteint plusieurs fonctionnalités : peut avoir une bordure, un texte
    sur plusieurs lignes.
    * Modifications dans le séquenceur pour utiliser cette nouvelle vue à la
    place des anciens textes flottants.

---
+++ src/components/FloatingText.vue
@@ -0,0 +1,40 @@
+<template>
+    <g :transform="`translate(${x}, ${y})`" :width="w" :height="h">
+        <rect
+            v-if="border !== ''"
+            :width="w"
+            :height="h"
+            :stroke="border"
+            :fill="background"
+            stroke-width="0.1"
+        />
+        <text :x="w / 2" :y="h / 2" v-if="content.indexOf('\n') > -1">
+            <tspan
+                v-for="line of content.split('\n')"
+                :key="line"
+                :x="w / 2"
+                dy="1"
+                >{{ line }}</tspan
+            >
+        </text>
+        <text :x="w / 2" :y="h / 2" v-else>
+            {{ content }}
+        </text>
+    </g>
+</template>
+
+<script>
+export default {
+    name: 'FloatingText',
+    props: {
+        x: Number,
+        y: Number,
+        w: Number,
+        h: Number,
+        border: String,
+        stroke: String,
+        background: String,
+        content: String,
+    },
+}
+</script>
