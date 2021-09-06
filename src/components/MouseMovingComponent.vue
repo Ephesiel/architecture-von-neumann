5203c11992d3d6e9e21b12225771e0de6935f5a1
Huftier Benoît <ephesiel@gmail.com>
2021-09-06 10:20:17 +0200

    Suppression descomposants spéarés

--- src/components/MouseMovingComponent.vue
+++ src/components/MouseMovingComponent.vue
@@ -1,15 +1,14 @@
 <template>
     <div
-        :style="`width: ${width}px;
-        height: ${height}px; 
-        overflow: ${overflow};`"
+        :style="`width: ${width}px; height: ${height}px;`"
         @mousedown="startDrag"
         @mousemove="doDrag"
+        @wheel="changeScale"
         class="moving-component-wrapper"
     >
         <div
-            :style="`width: 100%;
-            height: 100%;
+            :style="`width: ${sizes.componentWidth}px;
+            height: ${sizes.componentHeight}px;
             top: ${offset.top}px;
             left: ${offset.left}px;`"
             class="moving-component"
@@ -23,14 +22,31 @@ export default {
     props: {
         width: Number,
         height: Number,
-        overflow: { type: String, default: 'hidden' },
+        wheelSpeed: { type: Number, default: 10 },
+        maxScale: { type: Number, default: 200 },
+        minScale: { type: Number, default: 50 },
+        beginScale: { type: Number, default: 100 },
     },
     data() {
         return {
+            scale: this.beginScale,
             dragging: false,
             draggingPos: { x: 0, y: 0 },
             offset: { top: 0, left: 0 },
         }
+    },
+    computed: {
+        scaleRatio() {
+            return this.scale / 100
+        },
+        sizes() {
+            return {
+                height: this.height,
+                width: this.width,
+                componentHeight: this.height * this.scaleRatio,
+                componentWidth: this.width * this.scaleRatio,
+            }
+        },
     },
     methods: {
         startDrag(event) {
@@ -52,6 +68,32 @@ export default {
                 this.y = event.clientY
             }
         },
+        changeScale(event) {
+            this.scale -= event.deltaY * 0.0001 * this.wheelSpeed * this.scale
+            this.scale = Math.min(this.scale, this.maxScale)
+            this.scale = Math.max(this.scale, this.minScale)
+        },
+    },
+    watch: {
+        // Lorsque les tailles changent, on veut garder le même ratio du
+        // composant interne visible
+        // Si le changement implique un zoom, le zoom se fait depuis le centre
+        sizes: function (newSizes, oldSizes) {
+            // Les centres
+            const oldC = { x: oldSizes.width / 2, y: oldSizes.height / 2 }
+            const newC = { x: newSizes.width / 2, y: newSizes.height / 2 }
+
+            // Les pourcentage du composants situés en haut et à gauche du centre
+            const ratio = {
+                left: (oldC.x - this.offset.left) / oldSizes.componentWidth,
+                above: (oldC.y - this.offset.top) / oldSizes.componentHeight,
+            }
+
+            // Le point en haut à gauche du composant change pour garder les
+            // mêmes ratios en haut et à gauche du centre du cadre
+            this.offset.left = newC.x - ratio.left * newSizes.componentWidth
+            this.offset.top = newC.y - ratio.above * newSizes.componentHeight
+        },
     },
     mounted() {
         window.addEventListener('mouseup', this.stopDrag)
@@ -62,6 +104,7 @@ export default {
 <style lang="scss" scoped>
 .moving-component-wrapper {
     position: relative;
+    overflow: hidden;
 }
 
 .moving-component-wrapper .moving-component {
