8f0283ea7711c55db0ab8af116a343c5cc88f545
Huftier Benoît <ephesiel@gmail.com>
2021-09-09 08:42:49 +0200

    Correction de la page de debug qui avait une erreur
    - La page About est remplacé par le debug

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="nav">
         <router-link to="/">Home</router-link> |
-        <router-link to="/about">Debug</router-link>
+        <router-link to="/debug">Debug</router-link>
     </div>
     <router-view />
 </template>
